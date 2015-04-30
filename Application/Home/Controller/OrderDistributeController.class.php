f9d571d9122064b269bd5e83614387a42978fc8c
turbo <691209942@qq.com>
2015-04-30 11:45:56 +0800

    接口参数修改　 --钟明

--- Application/Home/Controller/OrderDistributeController.class.php
+++ Application/Home/Controller/OrderDistributeController.class.php
@@ -8,23 +8,54 @@ use UMessage\Logic\UnifyMessageSender;
 class OrderDistributeController extends Controller{
 
 
+    public function testAdd(){
+
+        $account = $_POST['user'][0]['account'];
+
+        $user = $_POST['user'];
+        $passengers = $_POST['passengers'];//array('amount' => '1', 'PassengerList' => array('0' => array('name' => '张鹏', 'ordertype' => 'adult', 'card_type' => '身份证', 'card_id' => '350622198510301039')));//
+        $passenger_amount = $_POST['passenger_amount'];
+        $need_ticket= $_POST['need_ticket'];
+        $phone = $_POST['phone'];
+        $flight_type = $_POST['flight_type'];
+        $flight = $_POST['flight'];//array('flight1' => array('flight_num' => 'MF8527', 'dcity' => '上海', 'acity' => '北京', 'dport' => '虹桥', 'aport' => '首都', 'dtime' => '2015-05-20 16:05:00', 'atime' => '2015-05-20 18:05:00', 'cabin_grade' => 'y', 'flight_price' => '500', 'adulttax' => '50', 'oilfee' => '50', 'acci_insu' => '50', 'delay_insu' => '50', 'total_price' => '720'));//
+        $total_price = $_POST['full_price'];
+
+
+        //生成 qsx订单号
+        $num = VNumGen('tmc_code');
+        $m_order = M('orders');
+        $data['order_num'] = $num;
+        $data['account'] = $account;
+        $data['phone'] = $phone;
+        $data['need_ticket'] = $need_ticket;
+        $data['passenger_amount'] =$passenger_amount;//
+        $data['refund_amount'] =0;//
+        $data['total_price'] =$total_price;//
+        $data['snatch_status'] = 0;
+        $data['ticket_status'] = 0;
+        $data['passengers'] = $passengers;
+        $data['flight'] = $flight;
+        $data['user'] = $user;
+        var_dump($data);
+    }
+
 	/*订票数据存储到数据库，短信通知客户
 	*author:zhangpeng
 	*
 	*/
 	public function orderdistribute()
 	{
-		$account = I("session.LoginInfo");//与LI方法等价
-		$order = $_POST['order'];
-		$order_info = json_decode($order, true);
-		$user = $order_info['user'];
-		$passengers = $order_info['passengers'];//array('amount' => '1', 'PassengerList' => array('0' => array('name' => '张鹏', 'ordertype' => 'adult', 'card_type' => '身份证', 'card_id' => '350622198510301039')));//
-		$passenger_amount = $passengers['amount'];
-		$need_ticket= $order_info['need_ticket'];
-		$phone = $order_info['phone'];
-		$flight_type = $order_info['flight_type'];
-		$flight = $order_info['flight'];//array('flight1' => array('flight_num' => 'MF8527', 'dcity' => '上海', 'acity' => '北京', 'dport' => '虹桥', 'aport' => '首都', 'dtime' => '2015-05-20 16:05:00', 'atime' => '2015-05-20 18:05:00', 'cabin_grade' => 'y', 'flight_price' => '500', 'adulttax' => '50', 'oilfee' => '50', 'acci_insu' => '50', 'delay_insu' => '50', 'total_price' => '720'));//
-		$total_price = $order_info['total_price'];
+		$account = $_POST['user'][0]['account'];
+
+		$user = $_POST['user'];
+		$passengers = $_POST['passengers'];//array('amount' => '1', 'PassengerList' => array('0' => array('name' => '张鹏', 'ordertype' => 'adult', 'card_type' => '身份证', 'card_id' => '350622198510301039')));//
+		$passenger_amount = $_POST['passenger_amount'];
+		$need_ticket= $_POST['need_ticket'];
+		$phone = $_POST['phone'];
+		$flight_type = $_POST['flight_type'];
+		$flight = $_POST['flight'];//array('flight1' => array('flight_num' => 'MF8527', 'dcity' => '上海', 'acity' => '北京', 'dport' => '虹桥', 'aport' => '首都', 'dtime' => '2015-05-20 16:05:00', 'atime' => '2015-05-20 18:05:00', 'cabin_grade' => 'y', 'flight_price' => '500', 'adulttax' => '50', 'oilfee' => '50', 'acci_insu' => '50', 'delay_insu' => '50', 'total_price' => '720'));//
+		$total_price = $_POST['full_price'];
 
 
 		//生成 qsx订单号
@@ -39,6 +70,7 @@ class OrderDistributeController extends Controller{
 		$data['total_price'] =$total_price;//
 		$data['snatch_status'] = 0;
 		$data['ticket_status'] = 0;
+
 		$order_add = $m_order->add($data);
 		//写入订单信息
 
@@ -46,9 +78,9 @@ class OrderDistributeController extends Controller{
 		if ($flight_type == 'S') {
 			//单程
 			for ($i = 0; $i < $passenger_amount; $i++) {
-				$flight['flight1']['passenger'] = $passengers['passengerList'][$i]['name'];
-				$flight['flight1']['id_type'] = $passengers['passengerList'][$i]['id_type'];
-				$flight['flight1']['card_id'] = $passengers['passengerList'][$i]['card_id'];
+				$flight['flight1']['passenger'] = $passengers[$i]['name'];
+				$flight['flight1']['id_type'] = $passengers[$i]['idType'];
+				$flight['flight1']['card_id'] = $passengers[$i]['idNumber'];
 				$flight['flight1']['account'] = $user['account'];
 				$flight['flight1']['order_num'] = $num;
 				$map = $flight['flight1'];
@@ -58,17 +90,17 @@ class OrderDistributeController extends Controller{
 			//往返行程
 			//去程
 			for ($i = 0; $i < $passenger_amount; $i++) {
-				$flight['flight1']['passenger'] = $passengers['passengerList'][$i]['name'];
-				$flight['flight1']['id_type'] = $passengers['passengerList'][$i]['id_type'];
-				$flight['flight1']['card_id'] = $passengers['passengerList'][$i]['card_id'];
+				$flight['flight1']['passenger'] = $passengers[$i]['name'];
+				$flight['flight1']['id_type'] = $passengers[$i]['idType'];
+				$flight['flight1']['card_id'] = $passengers[$i]['idNumber'];
 				$flight['flight1']['account'] = $user['account'];
 				$flight['flight1']['order_num'] = $num;
 				$map = $flight['flight1'];
 				$flight1_result = $m_flight->add($map);
 				//返程
-				$flight['flight2']['passenger'] = $passengers['passengerList'][$i]['name'];
-				$flight['flight2']['id_type'] = $passengers['passengerList'][$i]['id_type'];
-				$flight['flight2']['card_id'] = $passengers['passengerList'][$i]['card_id'];
+				$flight['flight2']['passenger'] = $passengers[$i]['name'];
+				$flight['flight2']['id_type'] = $passengers[$i]['idType'];
+				$flight['flight2']['card_id'] = $passengers[$i]['idNumber'];
 				$flight['flight2']['account'] = $user['account'];
 				$flight['flight2']['order_num'] = $num;
 				$flight2_result = $m_flight->add($flight['flight2']);
