fca5091e2e4b697d965258270deae745c338a197
mclaren <18065355@qq.com>
2015-07-01 10:41:22 +0800

    修改原因： 修改模块：paymentcontroller

--- Application/Home/Controller/PaymentController.class.php
+++ Application/Home/Controller/PaymentController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 
 use Think\Controller;
 use Org\Alipay;
+require 'C:/wamp/www/73go/ThinkPHP/Library/Org/ping/init.php';
 /**
  * 支付宝处理
  * 创建者：董发勇
@@ -22,6 +23,84 @@ class PaymentController extends Controller {
 	 * 修改者：王月
 	 * 修改时间：2015-3-25
 	 */
+	public function pingpay(){
+
+$input_data = json_decode(file_get_contents('php://input'), true);
+		$input_data['channel'] = 'alipay_wap';
+		$input_data['amount'] = '1';
+if (empty($input_data['channel']) || empty($input_data['amount'])) {
+	exit();
+}
+$channel = strtolower($input_data['channel']);
+$amount = $input_data['amount'];
+$orderNo = substr(md5(time()), 0, 12);
+
+//$extra 在使用某些渠道的时候，需要填入相应的参数，其它渠道则是 array() .具体见以下代码或者官网中的文档。其他渠道时可以传空值也可以不传。
+$extra = array();
+switch ($channel) {
+	//这里值列举了其中部分渠道的，具体的extra所需参数请参见官网中的 API 文档
+	case 'alipay_wap':
+		$extra = array(
+			'success_url' => 'http://www.yourdomain.com/success',
+			'cancel_url' => 'http://www.yourdomain.com/cancel'
+		);
+		break;
+	case 'upmp_wap':
+		$extra = array(
+			'result_url' => 'http://www.yourdomain.com/result?code='
+		);
+		break;
+	case 'bfb_wap':
+		$extra = array(
+			'result_url' => 'http://www.yourdomain.com/result?code='
+		);
+		break;
+	case 'upacp_wap':
+		$extra = array(
+			'result_url' => 'http://www.yourdomain.com/result?code='
+		);
+		break;
+	case 'wx_pub':
+		$extra = array(
+			'open_id' => 'Openid'
+		);
+		break;
+	case 'wx_pub_qr':
+		$extra = array(
+			'product_id' => 'Productid'
+		);
+		break;
+	case 'jdpay_wap':
+		$extra = array(
+			'success_url'=>'http://www.yourdomain.com',
+			'fail_url'=>'http://www.yourdomain.com'
+		);
+		break;
+
+}
+
+\Pingpp\Pingpp::setApiKey('sk_live_CejnL8nHm5i5fvLa5Cu5SiLC');
+try {
+	$ch = \Pingpp\Charge::create(
+		array(
+			"subject"   => "Your Subject",
+			"body"      => "Your Body",
+			"amount"    => '1',
+			"order_no"  => '123456',
+			"currency"  => "cny",
+			"extra"     => $extra,
+			"channel"   => $channel,
+			"client_ip" => '120.24.171.184',//$_SERVER["REMOTE_ADDR"],
+			"app"       => array("id" => "app_SuLWfHyXH0CCX1uL")
+		)
+	);
+	echo $ch;
+} catch (\Pingpp\Error\Base $e) {
+	header('Status: ' . $e->getHttpStatus());
+	echo($e->getHttpBody());
+}
+
+	}
 	public function payment(){
 		$account = I('session.account');
 		//find order infomation
